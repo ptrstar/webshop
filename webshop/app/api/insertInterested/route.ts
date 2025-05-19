50d25d25a2040a0f79ff3c95d722a43eb92e6287
Janosch Moor <janosch.moor@gmail.com>
2025-05-19 13:12:48 +0200

    fix build process

--- webshop/app/api/insertInterested/route.ts
+++ webshop/app/api/insertInterested/route.ts
@@ -7,6 +7,9 @@ export async function POST(req: Request) {
     await db.insert(interest).values({ email, amount, createdAt: new Date() });
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (err) {
-    return new Response(JSON.stringify({ error: "Database error" }), { status: 500 });
+    if (err) {
+      
+    }
+    return new Response(JSON.stringify({ error: "Database error: " }), { status: 500 });
   }
 }
