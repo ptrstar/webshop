e61e440dfa111c18c2dd259becc81f6af94d2d4d
Janosch Moor <janosch.moor@gmail.com>
2025-06-12 02:17:48 +0200

    auth in the box

--- app/api/get_all/route.ts
+++ app/api/get_all/route.ts
@@ -1,12 +1,20 @@
 import { db } from "@/util/supabase/drizzle";
 import { interest } from "@/util/supabase/schema";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/authOptions";
 
 export async function GET() {
-  try {
-    const data = await db.select().from(interest);
-    return new Response(JSON.stringify(data), { status: 200 });
-  } catch {
-    return new Response(JSON.stringify({ error: "Database error" }), { status: 500 });
+  const session = await getServerSession(authOptions);
+  if (session) {
+    try {
+      const data = await db.select().from(interest);
+      return new Response(JSON.stringify(data), { status: 200 });
+    } catch {
+      return new Response(JSON.stringify({ error: "Database error" }), { status: 500 });
+    }
+  } else {
+    return new Response(JSON.stringify({ error: "Access denied" }), { status: 401 });    
   }
+  
 }
 
