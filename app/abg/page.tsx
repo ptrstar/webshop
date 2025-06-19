319a953b768a0dbebd3fbaf8f56a224fed8d1133
Janosch Moor <janosch.moor@gmail.com>
2025-06-19 22:14:22 +0200

    agb and terms and conditions

---
+++ app/abg/page.tsx
@@ -0,0 +1,50 @@
+export default function Abg() {
+
+
+    return (
+        <div>
+
+            1. Geltungsbereich
+            Diese Allgemeinen Geschäftsbedingungen (AGB) gelten für alle Bestellungen, die über den Onlineshop von [Richi - das Kartenspiel] durch Privatpersonen mit Wohnsitz in der Schweiz getätigt werden. Mit der Vorbestellung gelten diese AGB als angenommen.
+            <br/>
+            2. Hinweis zur Verbindlichkeit der AGB
+            Der Onlineshop wird von zwei privaten Anbietern mit grossem Engagement, aber ohne juristische Ausbildung geführt. Wir bemühen uns, die AGB sowie die Datenschutzerklärung  laufend zu verbessern und zu professionalisieren. Sollte sich dennoch ein inhaltlicher oder formaler Fehler eingeschlichen haben, bitten wir um Nachsicht. Rechtliche Hinweise und Rückmeldungen nehmen wir gerne entgegen.
+            <br/>
+            3. Produkte
+            Es handelt sich um ein eigenes entwickeltes Kartenspiel, das in physischer Form zur Vorbestellung erhältlich ist. Design, Regeln und Inhalte unterliegen dem geistigen Eigentum von [Richi - das Kartenspiel].
+            <br/>
+            4. Vorbestellungen und Fairness
+            Das mutwillige Platzieren von Vorbestellungen ohne Kaufabsicht mit dem Ziel, künstlich eine Produktionsfreigabe auszulösen (bei Erreichen eines Mindestziels von 500 Bestellungen), ist untersagt. Sollten wir den Verdacht haben, dass Vorbestellungen nur zum Zweck abgegeben wurden, künstlich das Produktionsziel zu erreichen, ohne eine tatsächliche Kaufabsicht, behalten wir uns das Recht vor, diese Vorbestellungen zu überprüfen, und / oder die entsprechenden Vorbestellungen zu stornieren.
+            <br/>
+            5. Preise und Versandkosten
+            Der Preis von 14.90 ist in Schweizer Franken zu verstehen (CHF) exkl. Versandkosten. Die Versandkosten werden im Bestellprozess separat ausgewiesen.
+            <br/>
+            6. Zahlung
+            Die Zahlung kann auf der Webseite getätigt werden, sobald die Spiele produziert wurden.
+            <br/>
+            7. Lieferung
+            Die Lieferung erfolgt, nach dem Kauf eines Spieles.
+            <br/>
+            8. Widerrufsrecht / Rückgabe
+            Ein Rückgaberecht besteht nur bei beschädigter oder falsch gelieferter Ware. In diesem Fall bitten wir um Kontaktaufnahme innerhalb von 7 Tagen nach Erhalt der Lieferung.
+            <br/>
+            9. Haftung
+            Für Schäden, die durch unsachgemässe Verwendung des Produkts entstehen, übernehmen wir keine Haftung. Ebenso haften wir nicht für indirekte Schäden oder Folgeschäden.
+            <br/>
+            10. Datenschutz
+            Kundendaten werden vertraulich behandelt. Die Angabe der E-Mail-Adresse dient ausschliesslich dem Zweck, dich über Neuigkeiten zum Kartenspiel (z. B. Veröffentlichungstermin, Updates) zu informieren. Eine Weitergabe an Dritte erfolgt nicht. Mit der Angabe der E-Mail-Adresse erklärst du dich mit der Kontaktaufnahme durch [Richi - das Kartenspiel] einverstanden.
+            <br/>
+            11. Geistiges Eigentum
+            Sämtliche Inhalte (inkl. Spielidee, Design, Texte, Bilder) sind urheberrechtlich geschützt und dürfen ohne schriftliche Zustimmung von [Richi - das Kartenspiel] nicht verwendet oder vervielfältigt werden.
+            <br/>
+            12. Anwendbares Recht und Gerichtsstand
+            Es gilt ausschliesslich das Schweizer Recht. Gerichtsstand ist Basel-Stadt.
+            <br/>
+            13. Änderungen
+            Wir behalten uns vor, diese AGB bei Bedarf anzupassen, um sie an rechtliche oder technische Entwicklungen anzupassen oder unsere Angebote zu erweitern. Es gilt jeweils die auf unserer Website veröffentlichte Version.
+            Wir informieren auf der Website darüber, wenn eine neue Version veröffentlicht wurde.
+            Die neue AGB tritt jeweils 14 Tage nach ihrer Veröffentlichung in Kraft.
+
+        </div>
+    );
+}
---
+++ app/tac/page.tsx
@@ -0,0 +1,46 @@
+export default function tac() {
+
+    return (
+        <div>
+            1. Verantwortliche Stelle
+            Ort: 		 Riehen [4125], Schweiz
+            E-Mail:	 info@kartenspielrichi.ch
+            Website:	 https://www.kartenspielrichi.ch
+            <br/>
+            2. Allgemeines zur Datenverarbeitung
+            Wir erheben und verwenden personenbezogene Daten nur, soweit dies zur Bereitstellung eines funktionsfähigen Onlineshops sowie unserer Inhalte und Leistungen erforderlich ist. Die Erhebung und Verarbeitung erfolgt im Rahmen der gesetzlichen Vorschriften des Schweizer Datenschutzgesetzes (revDSG).
+            <br/>
+            3. Welche Daten wir speichern
+            Wenn in unserem Shop eine Vorbestellung erfolgt, erfassen wir folgende Daten:
+            * E-Mail-Adresse (zur Benachrichtigung über den Produktstart oder zur Kontaktaufnahme bei Verdacht, dass der vierte Punkt unserer AGB nicht eingehalten wurde)
+            <br/>
+            4. Zweck der Datenerhebung
+            Die Datenverarbeitung erfolgt zu folgenden Zwecken:
+            * Versand von Informationen zum Veröffentlichungszeitpunkt des Kartenspiels
+            * Kundenkommunikation bei Fragen
+            <br/>
+            5. Keine Weitergabe an Dritte
+            Deine personenbezogenen Daten werden nicht an Dritte weitergegeben. 
+            <br/>
+            6. Newsletter / Informations-E-Mails
+            Wenn du deine E-Mail-Adresse angibst, erklärst du dich damit einverstanden, von uns Informationen über den Produktstart zum Kartenspiel zu erhalten. 
+            <br/>
+            7. Speicherdauer
+            Personenbezogene Daten werden nur so lange gespeichert, wie dies zur Erfüllung der genannten Zwecke erforderlich ist oder wir gesetzlich zur Aufbewahrung verpflichtet sind.
+            <br/>
+            8. Deine Rechte
+            Du hast jederzeit das Recht auf:
+            * Auskunft über die gespeicherten Daten
+            * Löschung deiner Daten (sofern keine gesetzliche Aufbewahrungspflicht besteht)
+            Dazu genügt eine E-Mail an: [info@kartenspielrichi.ch]
+            <br/>
+            9. Datensicherheit
+            Wir schützen deine Daten durch geeignete technische und organisatorische Massnahmen, um sie vor unbefugtem Zugriff und Missbrauch zu schützen.
+            <br/>
+            10. Änderungen
+            Wir behalten uns vor, diese Datenschutzerklärung bei Bedarf anzupassen, um sie an rechtliche oder technische Entwicklungen anzupassen oder unsere Angebote zu erweitern. Es gilt jeweils die auf unserer Website veröffentlichte Version.
+            Wir informieren auf der Website darüber, wenn eine neue Version veröffentlicht wurde.
+            Die neue Datenschutzerklärung tritt jeweils 14 Tage nach ihrer Veröffentlichung in Kraft.
+        </div>
+    );
+}
